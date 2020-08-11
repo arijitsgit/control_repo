5d3cc7cfcd09b8465a051a8d2a8b1503fcf2ee0e
arijitsgit <arijit.pal456@gmail.com>
2020-08-11 19:04:44 -0400

    Update agent_nodes.pp

--- manifests/site/profile/manifests/agent_nodes.pp
+++ manifests/site/profile/manifests/agent_nodes.pp
@@ -1,5 +1,14 @@
-class profile::agent_nodes{
-include dockeragent
-dockeragent::node {'web.puppet.vm':}
-dockeragent::node {'db.puppet.vm':}
+class profile::agent_nodes {
+  include dockeragent
+  dockeragent::node { 'web.puppet.vm': }
+  dockeragent::node { 'db.puppet.vm': }
+  dockeragent::node { 'minetest.puppet.vm': }
+  host {‘web.puppet.vm’:
+    ensure => present,
+    ip => ‘10.0.2.15’,
+  }
+  host {‘db.puppet.vm’:
+    ensure => present,
+    ip => ‘10.0.2.2’,
+  }
 }
